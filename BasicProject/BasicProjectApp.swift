c9f6fc127301334825a0f85fd5e54d82e0953bf7
Linass3 <linas.venclavicius@gmail.com>
2025-03-14 00:21:22 +0200

    Added and initialized Firebase

--- BasicProject/BasicProjectApp.swift
+++ BasicProject/BasicProjectApp.swift
@@ -6,12 +6,32 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct BasicProjectApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor.systemGray5
+        UINavigationBar.appearance().backgroundColor = UIColor.systemYellow
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LoginView()
         }
     }
 }
